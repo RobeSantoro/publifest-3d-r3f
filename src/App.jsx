329c855433ef09a42e0dadc7c6bc330b55bc3691
RobeSantoro <santoro.robe@gmail.com>
2023-01-31 10:58:17 +0100

    Add Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import {Suspense} from 'react'
+import {Canvas} from '@react-three/fiber'
+import {Environment} from '@react-three/drei'
 import './App.css'
 
 function App() {
 
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <Canvas
+        shadows
+        dpr={[1, 2]}
+        cacamera={{
+          fov: 45,
+          near: 0.01,
+          far: 300,
+          position: [0, 1, 6]
+      }}
+      >
+        <ambientLight intensity={0.5} />
+        <Suspense fallback={null}>
+          {/* <Model scroll={scroll} /> */}
+          <Environment preset="city" />
+        </Suspense>
+      </Canvas>
+    </>
+  )
   
 }
 
