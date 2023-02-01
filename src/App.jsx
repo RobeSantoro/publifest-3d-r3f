d023cfc197341fa539cc929707d805e213571f15
RobeSantoro <santoro.robe@gmail.com>
2023-02-01 15:11:42 +0100

    Add ScrollControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 
+import * as THREE from 'three'
 import {Suspense, useEffect, useLayoutEffect} from 'react'
 import {Canvas, useFrame} from '@react-three/fiber'
 import {Html, ScrollControls, Sky, useScroll, useGLTF, useAnimations} from '@react-three/drei'
@@ -10,14 +11,15 @@ export default function App() {
 
   return (
     <>
-      <Canvas shadows dpr={[1, 2]} camera={{fov: 45, near: 0.01, far: 300, position: [0, 1, 6]}}>
+      <Canvas shadows dpr={[1, 2]} camera={{fov: 120, near: 0.01, far: 300, position: [0, 1, 0]}} gl={{alpha: false, antialias: false}}>
         <ambientLight intensity={0.5} />
+        <Sky sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
         {/* <OrbitControls dampingFactor={0.05} makeDefault target={[0, 1, 0]} /> */}
         <directionalLight position={[1, 2, 3]} intensity={1.5} />
 
         <Suspense fallback={Loading}>
-          <ScrollControls pages={3}>
-            <Congressi/>
+          <ScrollControls pages={10}>
+            <Congressi />
           </ScrollControls>
         </Suspense>
       </Canvas>
@@ -29,22 +31,38 @@ export default function App() {
 export function Congressi({...props}) {
 
   const scroll = useScroll()
-  const { scene, nodes, materials, animations } = useGLTF('/models/congressi-transformed.glb')
+  const {scene, nodes, materials, animations} = useGLTF('models/congressi-transformed.glb')
 
-  const { actions } = useAnimations(animations, scene)
+  const {actions} = useAnimations(animations, scene)
 
   useLayoutEffect(() => Object.values(nodes).forEach((node) => (node.receiveShadow = node.castShadow = true)))
-  useEffect(() => void (actions['CameraAction'].play().paused = true), [actions])
+
+  useEffect(() => {
+    console.log(`Camera Position: ${ nodes['Camera'].position } `)
+    console.log(`Camera Aim ${ ['Camera_Aim'].position }`)
+    console.log(`Scroll Offset: ${ scroll.offset }`)
+  }, [scroll, nodes])
+
+  useEffect(() => void (actions['Action.001'].play().paused = true), [actions])
+
   useFrame((state, delta) => {
-    const action = actions['CameraAction']
+
+    const action = actions['Action.001']
+
     // The offset is between 0 and 1, you can apply it to your models any way you like
-    const offset = 1 - scroll.offset
+    const offset = scroll.offset * 10
     action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * offset, 100, delta)
-    state.camera.position.set(Math.sin(offset) * -10, Math.atan(offset * Math.PI * 2) * 5, Math.cos((offset * Math.PI) / 3) * -10)
-    state.camera.lookAt(0, 0, 0)
+
+    const camera_null = nodes['Camera'].position
+    const camera_aim_null = nodes['Camera_Aim'].position
+
+    state.camera.lookAt(...camera_aim_null)
+    state.camera.position.set(...camera_null)
+
+    console.log(scroll.offset)
   })
 
   return <primitive object={scene} {...props} />
 }
 
-useGLTF.preload('/models/congressi-transformed.glb')
+useGLTF.preload('models/congressi-transformed.glb')
