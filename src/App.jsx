6a6f32d4360e3622b83c6a36f2b7578e1f0ce419
RobeSantoro <santoro.robe@gmail.com>
2023-02-01 09:44:08 +0100

    Add LitteTokyo Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
-import {Suspense} from 'react'
-import {Canvas} from '@react-three/fiber'
-import {Environment, Html} from '@react-three/drei'
 import './App.css'
 
-import Experience from './Experience'
+import {Suspense, useEffect, useLayoutEffect} from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
+import {Html, ScrollControls, Sky, useScroll, useGLTF, useAnimations} from '@react-three/drei'
 
 const Loading = <Html><div>LOADING...</div></Html>;
 
-function App() {
+export default function App() {
 
   return (
     <>
-      <Canvas shadows dpr={[1, 2]} camera={{ fov: 45,near: 0.01,far: 300,position: [0, 1, 6]}}>
+      <Canvas shadows dpr={[1, 2]} camera={{fov: 45, near: 0.01, far: 300, position: [0, 1, 6]}}>
+        <ambientLight intensity={0.5} />
+        {/* <OrbitControls dampingFactor={0.05} makeDefault target={[0, 1, 0]} /> */}
+        <directionalLight position={[1, 2, 3]} intensity={1.5} />
+
         <Suspense fallback={Loading}>
-          <Experience />
+          <ScrollControls pages={3}>
+            <Congressi/>
+          </ScrollControls>
         </Suspense>
       </Canvas>
     </>
   )
-  
+
 }
 
-export default App
+export function Congressi({...props}) {
+
+  const scroll = useScroll()
+  const { scene, nodes, materials, animations } = useGLTF('/models/congressi-transformed.glb')
+
+  const { actions } = useAnimations(animations, scene)
+
+  useLayoutEffect(() => Object.values(nodes).forEach((node) => (node.receiveShadow = node.castShadow = true)))
+  useEffect(() => void (actions['CameraAction'].play().paused = true), [actions])
+  useFrame((state, delta) => {
+    const action = actions['CameraAction']
+    // The offset is between 0 and 1, you can apply it to your models any way you like
+    const offset = 1 - scroll.offset
+    action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * offset, 100, delta)
+    state.camera.position.set(Math.sin(offset) * -10, Math.atan(offset * Math.PI * 2) * 5, Math.cos((offset * Math.PI) / 3) * -10)
+    state.camera.lookAt(0, 0, 0)
+  })
+
+  return <primitive object={scene} {...props} />
+}
+
+useGLTF.preload('/models/congressi-transformed.glb')
