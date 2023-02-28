3994d9793dabf8b1e90c495259d1522bcacd1a3f
RobeSantoro <santoro.robe@gmail.com>
2023-02-28 14:30:55 +0100

    Comment Stats

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       <Canvas shadows dpr={[1, 2]} gl={{alpha: true, antialias: false}} camera={{fov: 100, position: [0, 0, 0]}}>
         <ambientLight intensity={0.3} />
         <fog attach="fog" args={['white', 0.0001, 100]} />
-        <Stats />
+        {/* <Stats /> */}
         <Suspense fallback={Loading}>
           <ScrollControls pages={8} damping={0.2}>
 
