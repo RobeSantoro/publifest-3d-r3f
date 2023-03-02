070db7d684b0614faacf7584d97531f313be6f66
RobeSantoro <santoro.robe@gmail.com>
2023-03-02 17:42:24 +0100

    Add r3f-perf and Extract Camera Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import {Suspense, useRef} from 'react'
 import {Canvas} from '@react-three/fiber'
-import {Html, Scroll, ScrollControls, Stats, Environment} from '@react-three/drei'
+import {Html, Stats, Scroll, ScrollControls, Environment} from '@react-three/drei'
+import {Perf} from 'r3f-perf'
+
+import {Camera} from "./Camera";
 import {Meeting} from "./Meeting";
+
 import {Texts} from "./Texts";
 import Overlay from "./Overlay"
 
@@ -18,16 +22,20 @@ export default function App() {
       <Overlay ref={overlay} caption={caption} scroll={scroll} />
 
       <Canvas shadows dpr={[1, 2]} gl={{alpha: true, antialias: false}} camera={{fov: 100, position: [0, 0, 0]}}>
+        <Camera scroll={scroll} />
         <ambientLight intensity={0.3} />
         <fog attach="fog" args={['white', 0.0001, 100]} />
         <Stats />
+        <Perf />
         <Suspense fallback={Loading}>
+
           {/* <ScrollControls pages={7} damping={0.2}> */}
           <Meeting scroll={scroll} />
           {/* <Scroll html id="scroll" pages={7}>
               <Texts />
             </Scroll>
           </ScrollControls> */}
+
           <Environment preset="city" blur={0} background={false} />
         </Suspense>
       </Canvas>
--- src/Camera.jsx
+++ src/Camera.jsx
@@ -3,27 +3,32 @@ import {useEffect, useLayoutEffect} from 'react'
 import {useFrame} from '@react-three/fiber'
 import {useScroll, useGLTF, useAnimations} from '@react-three/drei'
 
-export function Meeting({...props}) {
+export function Camera({...props}) {
 
-    const {scene, nodes, materials, animations} = useGLTF('models/meeting-transformed.glb')
+    const {scene, nodes, animations} = useGLTF('models/camera-transformed.glb')
 
     const {scroll} = props
     // const scroll = useScroll()
 
     const {actions} = useAnimations(animations, scene)
 
-    useEffect(() => void (actions['meeting'].play().paused = true), [actions])
+    useEffect(() => void (actions['camera'].play().paused = true), [actions])
 
     useFrame((state, delta) => {
-        const action = actions['meeting']
+        const action = actions['camera']
 
         // const offset = scroll.offset * 10
         const offset = scroll.current * 10
 
         action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * offset, 8, delta)
+
+        const camera_null = nodes['Camera'].position
+        const camera_aim_null = nodes['Camera_Aim'].position
+        state.camera.lookAt(...camera_aim_null)
+        state.camera.position.set(...camera_null)
     })
 
     return <primitive object={scene} {...props} />
 }
 
-useGLTF.preload('models/meeting-transformed.glb')
+useGLTF.preload('models/camera-transformed.glb')
