8e849628b73a353541a04587de451badafbc9101
RobeSantoro <santoro.robe@gmail.com>
2023-03-07 11:27:24 +0100

    Move Fog to camera (to animate it)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Suspense, useRef, useEffect, useLayoutEffect} from 'react'
 import {Canvas} from '@react-three/fiber'
-import {Html, Stats, Scroll, useScroll, ScrollControls, Environment, MeshReflectorMaterial} from '@react-three/drei'
+import {Html, Stats, Scroll, useScroll, ScrollControls, Environment, MeshReflectorMaterial, useEnvLight} from '@react-three/drei'
 import {Perf} from 'r3f-perf'
 
 import Camera from "./Camera";
@@ -13,26 +13,26 @@ const Loading = <Html><div>LOADING...</div></Html>;
 
 export default function App() {
 
-  const scrollMultiplier = 6
+  const globalScrollMultiplier = 6
 
   return (
     <Canvas shadows dpr={[1, 2]} gl={{alpha: true, antialias: true}} camera={{fov: 100, position: [0, 0, 0]}}>
 
       <ambientLight intensity={0.3} />
-      <fog attach="fog" args={['white', 0.0001, 90]} />
+      {/* <fog attach="fog" args={['white', 0.0001, 90]} /> */}
 
       <Suspense fallback={Loading}>
         <ScrollControls pages={6} damping={0}>
+          <Camera multiplier={globalScrollMultiplier} />
+          <Meeting multiplier={globalScrollMultiplier} />
+          <Ground multiplier={globalScrollMultiplier} />
           <Texts />
-          <Camera multiplier={scrollMultiplier} />
-          <Meeting multiplier={scrollMultiplier} />
-          <Ground multiplier={scrollMultiplier} />
         </ScrollControls>
         <Environment preset="city" blur={0} background={false} />
       </Suspense>
 
-      <Stats />
-      <Perf />
+      {/* <Stats /> */}
+      {/* <Perf /> */}
 
     </Canvas>
   )
--- src/Camera.jsx
+++ src/Camera.jsx
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import {useEffect, useLayoutEffect} from 'react'
 import {useFrame} from '@react-three/fiber'
 import {useScroll, useGLTF, useAnimations} from '@react-three/drei'
@@ -5,7 +6,8 @@ import {useScroll, useGLTF, useAnimations} from '@react-three/drei'
 export default function Camera(props) {
 
     const scroll = useScroll()
-    
+    let fogFar = 70
+
     const {scene, nodes, animations} = useGLTF('models/camera-transformed.glb')
     const {actions} = useAnimations(animations, scene)
 
@@ -16,11 +18,20 @@ export default function Camera(props) {
         const camera_aim_null = nodes['Camera_Aim'].position
         state.camera.lookAt(...camera_aim_null)
         state.camera.position.set(...camera_null)
+        state.camera.updateProjectionMatrix()
 
-        actions['camera'].time = scroll.offset * props.multiplier
+        const action = actions['camera']
+        action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * scroll.offset * props.multiplier, 8, delta)
+
+        fogFar = THREE.MathUtils.lerp(10, 90, scroll.offset*100)
     })
 
-    return <primitive object={scene} props />
+    return (
+        <>
+            <primitive object={scene} props />
+            <fog attach="fog" args={['white', 0.0001, fogFar]} />
+        </>
+    )
 }
 
 useGLTF.preload('models/camera-transformed.glb')
