3926c878c024b8c8d9cf649854569613b663d98c
RobeSantoro <santoro.robe@gmail.com>
2023-04-24 14:59:29 +0200

    Update Cerimonie

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
         frameloop='always'
         dpr={[1, 2]}
         gl={{ alpha: true, antialias: true, preserveDrawingBuffer: true }}
-        camera={{ fov: 100, position: [0, 0, 10], near: 1, far: 1000 }}>
+        camera={{ fov: 100, position: [0, 0, 10000], near: 1, far: 1000 }}>
         <ambientLight intensity={0.3} />
         <Suspense fallback={<Loading />}>
           <World />
--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,29 +1,38 @@
 import * as THREE from 'three'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { useScroll, useGLTF, useAnimations } from '@react-three/drei'
 
 export default function Camera(props) {
 
     const scrollData = useScroll()
+    const firstFrame = useRef(true)
 
     const { scene, nodes, animations } = useGLTF('/camera-transformed.glb')
     const { actions } = useAnimations(animations, scene)
 
-    useEffect(() => void (actions['camera'].play().paused = true), [actions])
+    useEffect(() => {
+        const action = actions['camera']
+        action.play()
+        action.paused = true
+    }, [actions])
 
     useFrame(({ mouse, camera }) => {
-        const action = actions['camera']
-        action.time = scrollData.offset * props.multiplier
+        if (!firstFrame.current) {
+            const action = actions['camera']
+            action.time = scrollData.offset * props.multiplier
 
-        const camera_null = nodes['Camera_Pos'].position
-        const camera_aim_null = nodes['Camera_Aim'].position
+            const camera_null = nodes['Camera_Pos'].position
+            const camera_aim_null = nodes['Camera_Aim'].position
 
-        camera.position.x = camera_null.x
-        camera.position.y = camera_null.y
-        camera.position.z = camera_null.z
+            camera.position.x = camera_null.x
+            camera.position.y = camera_null.y
+            camera.position.z = camera_null.z
 
-        camera.lookAt(...camera_aim_null)
+            camera.lookAt(...camera_aim_null)
+        }
+
+        firstFrame.current = false
     })
 
     return <primitive object={scene} props />
